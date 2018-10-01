b3e2eccb9b7983fc4c7c5b1975ea60307e79e041
SergeySkidan <SergeySkidan1982@gmail.com>
2018-10-01 21:56:27 +0300

    building a project, add new file

---
+++ my-app/src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+
+<h1>FFFFFrerererFF</h1>
+        </header>
+<h1>FFFsdvssvFFFF</h1>
+
+<h1>FjccccccccwwwwwwwwfcjfFFF</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
